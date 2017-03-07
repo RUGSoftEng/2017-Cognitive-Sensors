f9b50f11ca05a7397ceb63296867d16c8397b819
A D Spina <a.d.spina@student.rug.nl>
2017-03-07 14:17:01 +0100

    Added Debug menu, Made game functioning sans Mood Text,  Added notifications
    Debug menu accessible by tapping copyright image 3 times.  Game now
    works more as intended.  TODO:: change game to Fragment and put mood
    questions.  Added notifications for a set time and also an ability to
    send one from the debug menu.  Please do testing.

--- Wander/app/src/main/java/com/teamwan/wander/MainMenu.java
+++ Wander/app/src/main/java/com/teamwan/wander/MainMenu.java
@@ -1,10 +1,17 @@
 package com.teamwan.wander;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.content.Intent;
+
+import java.util.Calendar;
 
 
 /**
@@ -31,15 +38,52 @@ public class MainMenu extends AppCompatActivity {
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     private View mContentView;
+    private int countClicks;
+    private PendingIntent pendingIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main_menu);
+        countClicks = 0;
+
+        //if this is the first time the app has started setup the notifications, else don't do anything
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!prefs.getBoolean("firstTime", false))
+            setUpNotifications();
     }
+
     //Start a new activity on click of the Start Button
-    public void onClickStart(View v){
+    public void onClickStart(View v) {
         Intent intent = new Intent(MainMenu.this, NumberGame.class);
         MainMenu.this.startActivity(intent);
+    }
+    //click the copyright image 3 times to start the debug menu
+    public void onClickDebug(View v){
+        ++countClicks;
+        if(countClicks >= 3)
+        {
+            Intent intent = new Intent(MainMenu.this, DebugActivity.class);
+            MainMenu.this.startActivity(intent);
+            countClicks = 0;
+        }
+    }
+    //this sets up the notification to fire at the time set in the calendar
+    //TODO:: test that this actually works
+    public void setUpNotifications(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 10);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 1);
+
+        Intent alarmIntent = new Intent(this, MyReceiver.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
     }
 }
--- Wander/app/src/main/java/com/teamwan/wander/NumberGame.java
+++ Wander/app/src/main/java/com/teamwan/wander/NumberGame.java
@@ -9,6 +9,8 @@ after gameLength seconds.
 
 package com.teamwan.wander;
 
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,36 +18,102 @@ import android.widget.TextView;
 
 import java.util.Random;
 
+import static android.R.attr.textColor;
 import static java.lang.Math.abs;
 
 public class NumberGame extends AppCompatActivity {
 
-    private TextView numberDisplay;
-    private Random rn;
-    private long startTime;
-    private long gameLength = 10; //game duration in seconds for testing
+    private TextView numberDisplay; //where the number is shown
+    private Random rn;//random number generator
+    private long startTime;//game start time
+    private long timeLastClicked;//last time the number was clicked
+    private final long gameLength = 30 * 1000; //game duration in seconds for testing
+
+    private final int unClickableNum = 3;//number which should not be clicked
+    private int currentNum;//current displayed number
+    private int successValue;//state of game represented as -1 | 0 | 1 == failure |  nothing | success
+
+    private int successCounter;
+    private int failCounter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_game);
 
-        runGame();
+        runGame();//setup the game values
+
+        final Handler handle = new Handler();
+        final long delay = 650;
+        //This handler checks if the number has been successfully interacted with each delay period
+        handle.postDelayed(new Runnable(){
+            public void run(){
+                if(System.currentTimeMillis() - timeLastClicked > delay)
+                    checkSuccess();
+
+                handle.postDelayed(this, delay);
+            }
+        }, delay);
     }
     //This function initializes the objects needed to run the game
     protected void runGame(){
         startTime = System.currentTimeMillis();
+        timeLastClicked = System.currentTimeMillis();
+
+        successCounter = 0;
+        failCounter = 0;
+
         numberDisplay = (TextView) findViewById(R.id.numberDisplay);
         rn = new Random();
-        int i = rn.nextInt() % 9;
-        numberDisplay.setText(Integer.toString(abs(i)));//set text in textView as the generated integer
+        genNewNumber();
+
     }
+
     //Puts a new integer in the textView when the TextView is clicked
     public void onClickNumber(View v){
-        int i = rn.nextInt() % 9;
-        numberDisplay.setText(Integer.toString(abs(i)));
+        if(currentNum == unClickableNum)
+            failOnNum();
+        else
+            successOnNum();
+        timeLastClicked = System.currentTimeMillis();
+    }
+    //actions taken if the player does things wrong
+    private void failOnNum(){
+        numberDisplay.setTextColor(getResources().getColor(R.color.negativeResult));
+        successValue = -1;
+        ++failCounter;
+    }
+    //actions taken if the player does things successfully
+    private void successOnNum(){
+        ++successCounter;
+        numberDisplay.setTextColor(getResources().getColor(R.color.positiveResult));
+        successValue = 1;
         //finish activity after gameLength seconds and return to mainMenu which is on top of the activity stack
-        if(System.currentTimeMillis() - startTime > gameLength * 1000)
+        if(System.currentTimeMillis() - startTime > gameLength)
             finish();
+    }
+
+    private void checkSuccess(){
+        if(successValue != 0){//if something has happened
+            if (successValue == 1)//if previous action was successful
+                genNewNumber();
+            else//This else lets the delay last a little longer so they can experience their failure.
+                successValue = 1;
+        }
+        else{
+            if(currentNum == unClickableNum && System.currentTimeMillis() - timeLastClicked > 2000)
+                successOnNum();//
+            // if the number is the one that should not be clicked and they've not touched it for 2 seconds they are successful time can be changed though
+        }
+    }
+    //generates a new number and resets the text appearance
+    private void genNewNumber(){
+        long hold = currentNum;
+        while(hold == currentNum)//ensure new value is different from old one.
+            currentNum = abs(rn.nextInt() % 9);
+        numberDisplay.setText(Integer.toString(currentNum));//set text in textView as the generated integer
+        numberDisplay.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        successValue = 0;
     }
 }
