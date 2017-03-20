b390cbf65192cccfad00fea9e4baedb915a1ef82
A D Spina <a.d.spina@student.rug.nl>
2017-03-20 15:59:37 +0100

    Not Working Version
    Started adding layouts for asking questions and modifying game to start
    those activities.  There's a definite issue with the game timing still.

--- Wander/app/src/main/java/com/teamwan/wander/MainMenu.java
+++ Wander/app/src/main/java/com/teamwan/wander/MainMenu.java
@@ -130,8 +130,6 @@ public class MainMenu extends AppCompatActivity {
         accept.setVisibility(View.VISIBLE);
         quit.setVisibility(View.VISIBLE);
 
-
-
         Typeface tf =Typeface.createFromAsset(getAssets(),"fonts/FuturaLT.ttf");
         title.setTypeface(tf);
         body.setTypeface(tf);
--- Wander/app/src/main/java/com/teamwan/wander/NumberGame.java
+++ Wander/app/src/main/java/com/teamwan/wander/NumberGame.java
@@ -9,6 +9,7 @@ after gameLength seconds.
 
 package com.teamwan.wander;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Handler;
@@ -18,11 +19,9 @@ import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import static android.R.attr.textColor;
 import static java.lang.Math.abs;
 
 public class NumberGame extends AppCompatActivity {
@@ -30,18 +29,19 @@ public class NumberGame extends AppCompatActivity {
     private TextView numberDisplay; //where the number is shown
     private Random rn;//random number generator
     private long startTime;//game start time
-    private long timeLastClicked;//last time the number was clicked
-    private long timeNumberDisplayed;//Time last number was displayed
     private long gameLength; //game duration in seconds for testing
-
+    private int amountOfQuestions;
     private final int unClickableNum = 3;//number which should not be clicked
     private int currentNum;//current displayed number
     private int successValue;//state of game represented as -1 | 0 | 1 == failure |  nothing | success
+    private int questionID;
+    private ArrayList<QuestionInfo> questionIntervals;
 
     private int successCounter;
     private int failCounter;
+    private long timeLastClicked;//last time the number was clicked
+    private long timeNumberDisplayed;//Time last number was displayed
     private RelativeLayout rl;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +54,9 @@ public class NumberGame extends AppCompatActivity {
         numberDisplay.setTypeface(tf);
 
         rl = (RelativeLayout)findViewById(R.id.gameUI);
-
+        questionID = 0;
         gameLength = (getResources().getInteger(R.integer.game_length)) * 1000;
+        amountOfQuestions = getResources().getInteger(R.integer.amount_questions);
         final long delay = (getResources().getInteger(R.integer.number_display)) * 1000;
         final long jitter = (getResources().getInteger(R.integer.jitter_range)) * 100;
 
@@ -80,6 +81,9 @@ public class NumberGame extends AppCompatActivity {
         successCounter = 0;
         failCounter = 0;
 
+        questionIntervals = new ArrayList<QuestionInfo>();
+        questionIntervals.add(new QuestionInfo(0, 2));//TODO:: add all questions here using this example
+
         numberDisplay = (TextView) findViewById(R.id.numberDisplay);
         rn = new Random();
         genNewNumber();
@@ -101,6 +105,7 @@ public class NumberGame extends AppCompatActivity {
         rl.setBackgroundColor(getResources().getColor(R.color.negativeResult));
         successValue = 1;
         ++failCounter;
+        System.out.println("fail");
         numberDisplay.setText("");
     }
 
@@ -109,6 +114,7 @@ public class NumberGame extends AppCompatActivity {
         rl.setBackgroundColor(getResources().getColor(R.color.positiveResult));
         successValue = 1;
         ++successCounter;
+        System.out.println("success");
         numberDisplay.setText("");
 
         if(System.currentTimeMillis() - startTime > gameLength) {
@@ -119,16 +125,42 @@ public class NumberGame extends AppCompatActivity {
 
     private void checkSuccess(){
         // if the number is the one that should not be clicked and they've not touched it for 3 seconds they are successful time can be changed to a set value
-        if(currentNum == unClickableNum && successValue == 0)
+        if(currentNum == unClickableNum && successValue == 0){
+            System.out.println("success in check");
             ++successCounter;
+        }
+
         //number was not clicked when it should have been so it is a failure
-        else
+        else if(currentNum == unClickableNum && successValue == 1){
+            System.out.println("fail in check");
             ++failCounter;
+        }
+
+        //after a certain amount of numbers ask a question
+        System.out.println(successCounter + failCounter);
+        if(questionID < questionIntervals.size() && (successCounter + failCounter) >= questionIntervals.get(questionID).getQuestionOrder()){
+            Intent intent;
+            if(questionIntervals.get(questionID).getQuestionType() == 1)
+                intent = new Intent(getApplicationContext(), InGameSliderQuestion.class);
+            else
+                intent = new Intent(getApplicationContext(), InGameMultiQuestion.class);
+
+            intent.putExtra("questionID", questionID);
+            startActivityForResult(intent, 1);
+            ++questionID;
+            //TODO::pause current activity
+        }
+        else{
+            genNewNumber();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         genNewNumber();
     }
 
     //generates a new number and resets the text appearance
-    private void genNewNumber(){
+    public void genNewNumber(){
         saveLastNumberData();
 
         rl.setBackgroundColor(Color.parseColor("#FFFFFF"));
@@ -158,8 +190,6 @@ public class NumberGame extends AppCompatActivity {
                 -failCounter == unsuccessful clicks
                 -goodNum == TRUE if last displayed number was a clickable one, else FALSE
          */
-
-
     }
 
 }
