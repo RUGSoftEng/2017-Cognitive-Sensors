983d157f544674132ee6e1e5f7590acfe814c1c4
aukeroorda <aukeroorda@gmail.com>
2017-03-22 10:48:07 +0100

    Actually added the right files now.

---
+++ Wander/app/src/main/java/com/teamwan/wander/GameSession.java
@@ -0,0 +1,91 @@
+package com.teamwan.wander;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+public class GameSession{
+
+    private int gameSessionId;
+    private int playerId;
+    private String time;
+    private String gameType;
+    private ArrayList<NumberGuess> numberGuesses;
+    private ArrayList<String> questions;
+    private  int answer;
+
+
+    public GameSession(String time, String gameType) {
+        this.time = time;
+        this.gameType = gameType;
+    }
+
+    public int getGameSessionId() {
+        return gameSessionId;
+    }
+
+    public void setGameSessionId(int gameSessionId) {
+        this.gameSessionId = gameSessionId;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getGameType() {
+        return gameType;
+    }
+
+    public void setGameType(String gameType) {
+        this.gameType = gameType;
+    }
+
+    public ArrayList<NumberGuess> getNumberGuesses() {
+        return numberGuesses;
+    }
+
+    public void setNumberGuesses(ArrayList<NumberGuess> numberGuesses) {
+        this.numberGuesses = numberGuesses;
+    }
+
+    public void addNumberGuess(NumberGuess ng){
+        this.numberGuesses.add(ng);
+    }
+
+    public ArrayList<String> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<String> questions) {
+        this.questions = questions;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public void save(Context context){
+        DBHelper dbHelper = new DBHelper(context);
+        dbHelper.openDB();
+
+
+        dbHelper.closeDB();
+    }
+
+}
---
+++ Wander/app/src/main/java/com/teamwan/wander/NumberGuess.java
@@ -0,0 +1,57 @@
+package com.teamwan.wander;
+
+public class NumberGuess {
+
+    private int number;
+    private boolean isGo;
+    private boolean correct;
+    private long responseTime;
+    private long time;
+
+
+    public NumberGuess(int number, boolean isGo, long time) {
+        this.number = number;
+        this.isGo = isGo;
+        this.time = time;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isGo() {
+        return isGo;
+    }
+
+    public void setGo(boolean go) {
+        isGo = go;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public long getResponseTime() {
+        return responseTime;
+    }
+
+    public void setResponseTime(long responseTime) {
+        this.responseTime = responseTime;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
